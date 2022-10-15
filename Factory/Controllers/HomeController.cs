1a308f131ff5ed5199838936b1595f00bc1fe6d3
Harold_Mesa <harold.mesa@icloud.com>
2022-10-14 20:29:25 -0500

    Add baseline requirements for the Independent Project.

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,14 +1,26 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Factory.Models;
 
 namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly FactoryContext _db;
+
+      public HomeController(FactoryContext db)
+      {
+        _db = db;
+      }
 
       [HttpGet("/")]
       public ActionResult Index()
       {
-        return View();
+        ViewBag.Machine = new List<Machine>( _db.Machines);
+        return View(_db.Engineers.ToList());
       }
 
     }
